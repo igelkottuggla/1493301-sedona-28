3195b6bd2b928bbd16b28e2607ba0673436ff94f
igelkottuggla <rot.fuchslein@gmail.com>
2020-07-12 20:41:43 +0500

    Начинаем оформление декоративных элементов

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,15 +1,22 @@
+
 var searchOpen = document.querySelector(".searh-form-btn");
 var searchForm = document.querySelector(".search-form");
 
-if (searchOpen) {
+
+  searchOpen.addEventListener("click", function (event) {
+    event.preventDefault();
+    searchForm.classList.toggle("search-form-hide");
+    searchForm.classList.toggle("search-form-show");
+  });
+
+/* if (searchOpen) {
   if (searchForm) {
     searchForm.classList.add("search-form-hide");
   };
 
- searchOpen.addEventListener("click", function(event) {
+  searchOpen.addEventListener("click", function (event) {
     event.preventDefault();
-    searchForm.classList.toggle("search-form-show");
     searchForm.classList.toggle("search-form-hide");
+    searchForm.classList.toggle("search-form-show");
   });
-};
-
+};*/
